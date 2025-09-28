249eb0ad8b503e2370f2eac3758970a461aab13b
harishkv.qa@gmail.com <harishkv.qa@gmail.com>
2025-09-28 09:08:52 +0000

    1

--- models/Staging/customers.sql
+++
@@ -1,9 +0,0 @@
-RAW_CUSTOMER{{ config(materialized='table') }}
-
-WITH tb1 as(
-    select
-    id as cust_id,
-    first_name as name,
-    last_name
-    from {{source('EM','RAW_CUSTOMER')}})
-select * from tb1 
